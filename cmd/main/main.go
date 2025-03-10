fa7882f57fb3a91e868627c35e202cc8b606c8b9
ntwalibrian <ntwalibriand@gmail.com>
2025-03-10 07:17:40 +0200

    fix root directory 404 error

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -7,8 +7,14 @@ import (
 	"github.com/gorilla/mux"
 )
 
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello vercel by diouf ")
+}
+
 func main() {
 	r := mux.NewRouter()
+
+	r.HandleFunc("/", helloHandler).Methods("GET")
 
 	fmt.Printf("Starting server at port 8080 or on vercel \n")
 	log.Fatal(http.ListenAndServe(":8080", r))
