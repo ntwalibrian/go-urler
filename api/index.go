a8f285364b5f3d8bb8805530fd799ac1e1e71b89
ntwalibrian <ntwalibriand@gmail.com>
2025-03-10 07:41:29 +0200

    vercel changes 3

--- api/index.go
+++ api/index.go
@@ -7,14 +7,14 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
+func Handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Hello vercel by diouf </h1>")
 }
 
 func main() {
 	r := mux.NewRouter()
 
-	r.HandleFunc("/", helloHandler).Methods("GET")
+	r.HandleFunc("/", Handler).Methods("GET")
 
 	fmt.Printf("Starting server at port 8080 or on vercel \n")
 	log.Fatal(http.ListenAndServe(":8080", r))
